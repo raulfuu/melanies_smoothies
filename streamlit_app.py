ed0b05f7b9c29066b7150bb9a8fd506c747cbbd4
raulfuu <raulfa03@gmail.com>
2026-06-02 09:43:24 +0200

    Integrate pandas for DataFrame handling and updates
    Added pandas for DataFrame conversion and updated the multiselect box to use a Pandas DataFrame. Modified API call to use the SEARCH_ON value for fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests  
+import pandas as pd # <-- Added pandas
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -15,24 +16,32 @@ st.write('The name of your smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-# session = get_active_session() 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
-# Show the multiselect box
+# Pull BOTH columns from Snowflake
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+
+# Show the multiselect box (using just the FRUIT_NAME column for the UI)
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:', my_dataframe, max_selections=5
+    'Choose up to 5 ingredients:', pd_df['FRUIT_NAME'], max_selections=5
 )   
 
 if ingredients_list:
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        
+        # Look up the exact SEARCH_ON value for the fruit the user chose
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        
+        # Swap out 'fruit_chosen' for 'search_on' in the API URL
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 
     # Build the insert statement
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
